d1aa8ceb70974c7194eaabc51afa3241f8a314cb
modelkit62 <modelkit62@gmail.com>
2023-10-15 21:41:19 +0100

    Initial commit

---
+++ src/main/java/com/example/oshit/OshitApplication.java
@@ -0,0 +1,13 @@
+package com.example.oshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oshit/OshitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oshit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
