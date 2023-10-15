c09e44f75fb4dfa8b5e6b54c4b2177667e8d314c
modelkit62 <modelkit62@gmail.com>
2023-10-15 22:02:53 +0100

    tc

--- src/main/java/com/example/oshit/OshitApplication.java
+++ src/main/java/com/example/oshit/OshitApplication.java
@@ -4,18 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/app")
 public class OshitApplication {
 
-	@GetMapping("/")
 	public String getName(){
 		return "Hijoeputa!";
 	}
 
-	@GetMapping("/error")
+	@GetMapping("/cabron")
 	public String getError(){
 		return "Hijoeputa ERROR!";
 	}
