603063752deeced0b6cc63274ba1fa7cf38530d5
modelkit62 <modelkit62@gmail.com>
2023-10-15 22:10:15 +0100

    fc

--- src/main/java/com/example/oshit/OshitApplication.java
+++ src/main/java/com/example/oshit/OshitApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app")
 public class OshitApplication {
 
+	@GetMapping("/")
 	public String getName(){
 		return "Hijoeputa!";
 	}
